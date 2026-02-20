464beea6b6b89267665124af0fa42ff89ee197f9
Brian C. Van Essen <vanessen1@llnl.gov>
2026-02-19 17:04:40 -0800

    Added SMILES and SMARTS servers.

---
+++ flask_mcp/chemistry/SMARTS_reactions.py
@@ -0,0 +1,45 @@
+################################################################################
+## Copyright 2025 Lawrence Livermore National Security, LLC. and Binghamton University.
+## See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+################################################################################
+
+from mcp.server.fastmcp import FastMCP
+from loguru import logger
+
+try:
+    from rdkit import Chem
+    from rdkit.Chem import AllChem, rdChemReactions
+
+    HAS_SMARTS = True
+except (ImportError, ModuleNotFoundError) as e:
+    HAS_SMARTS = False
+    logger.warning(
+        "Please install the rdkit support packages to use this module."
+        "Install it with: pip install charge[rdkit]",
+    )
+
+from charge.servers.server_utils import add_server_arguments
+import argparse
+
+parser = argparse.ArgumentParser()
+add_server_arguments(parser)
+args = parser.parse_args()
+
+SMARTS_mcp = FastMCP(
+    "[RDKit-SMARTS] Chemistry and reaction verification MCP Server",
+    port=args.port,
+    website_url=f"{args.host}",
+)
+
+logger.info("[RDKit-SMARTS] Starting Chemistry and reaction verification MCP Server")
+
+import charge.servers.SMARTS_reactions_utils as smarts
+import charge.servers.SMILES_utils as smiles
+
+SMARTS_mcp.tool()(smarts.verify_reaction_SMARTS)
+
+SMARTS_mcp.tool()(smiles.verify_smiles)
+
+SMARTS_mcp.tool()(smarts.verify_reaction)
---
+++ flask_mcp/chemistry/SMILES.py
@@ -0,0 +1,47 @@
+################################################################################
+## Copyright 2025 Lawrence Livermore National Security, LLC. and Binghamton University.
+## See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+################################################################################
+
+from mcp.server.fastmcp import FastMCP
+from loguru import logger
+
+try:
+    from rdkit import Chem
+    from rdkit.Chem import AllChem, Descriptors
+    from rdkit.Contrib.SA_Score import sascorer
+
+    HAS_SMILES = True
+except (ImportError, ModuleNotFoundError) as e:
+    HAS_SMILES = False
+    logger.warning(
+        "Please install the rdkit support packages to use this module."
+        "Install it with: pip install charge[rdkit]",
+    )
+
+from charge.servers.server_utils import add_server_arguments
+import argparse
+
+parser = argparse.ArgumentParser()
+add_server_arguments(parser)
+args = parser.parse_args()
+
+SMILES_mcp = FastMCP(
+    "[RDKit-SMILES] Chem and BioInformatics MCP Server",
+    port=args.port,
+    website_url=f"{args.host}",
+)
+
+logger.info("[RDKit-SMILES] Starting Chem and BioInformatics MCP Server")
+
+import charge.servers.SMILES_utils as smiles
+
+SMILES_mcp.tool()(smiles.canonicalize_smiles)
+
+SMILES_mcp.tool()(smiles.verify_smiles)
+
+SMILES_mcp.tool()(smiles.get_synthesizability)
+
+SMILES_mcp.tool()(smiles.known_smiles)
